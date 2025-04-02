189193382a6f6840aeec4db6455d5c12a02c37cf
christian-word <ruyriy@gmail.com>
2025-04-02 15:21:43 +0200

    Update HarmonyPlayer.js

--- src/core/HarmonyPlayer.js
+++ src/core/HarmonyPlayer.js
@@ -1,1 +1,14 @@
+connectedCallback() {
+    const type = this.getAttribute('type');
+    
+    if (type === 'youtube') {
+        const videoId = this.getAttribute('video-id');
+        this.loadYouTube(videoId);
+    }
+}
 
+loadYouTube(videoId) {
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=0`;
+    this.shadowRoot.querySelector('.player').appendChild(iframe);
+}
